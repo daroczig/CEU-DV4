5f9192722314771e8898a4b1a800c8d9ef62a896
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2020-05-23 00:52:37 +0200

    countdown

--- apps/countdown/server.R
+++ apps/countdown/server.R
@@ -1,14 +1,15 @@
 library(shiny)
+library(lubridate)
 
 title <- 'Data Visualization 3'
 subtitle <- 'Data Visualization in Production with Shiny'
-
+schedule <- as.POSIXct('2020-05-25 13:30:00')
 
 server <- function(input, output) {
     output$title <- renderText(title)
     output$subtitle <- renderText(subtitle)
     output$countdown <- renderText({
-        invalidateLater(250)
-        Sys.time()
+        invalidateLater(500)
+        as.character(round(as.period(schedule - Sys.time())))
     })
 }
