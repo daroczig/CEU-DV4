4ca27b014d07bb878ee24931dcba3c50880d6fe8
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2023-04-23 11:44:50 +0200

    add current time

--- apps/countdown/server.R
+++ apps/countdown/server.R
@@ -6,4 +6,5 @@ subtitle <- 'Data Visualization in Production with Shiny'
 server <- function(input, output) {
     output$title <- renderText(title)
     output$subtitle <- renderText(subtitle)
+    output$countdown <- renderText(Sys.time())
 }
--- apps/countdown/ui.R
+++ apps/countdown/ui.R
@@ -2,5 +2,6 @@ library(shiny)
 
 ui <- basicPage(
     h1(uiOutput('title')),
-    h2(uiOutput('subtitle'))
+    h2(uiOutput('subtitle')),
+    p(uiOutput('countdown'))
 )
