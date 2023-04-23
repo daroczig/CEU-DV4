6eec99138b200b0d58cf19301be16a847f2f7808
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2023-04-23 13:29:39 +0200

    add support for timezone setting

--- apps/countdown/server.R
+++ apps/countdown/server.R
@@ -7,21 +7,23 @@ server <- function(input, output, session) {
     settings <- reactiveValues(
         title = 'Data Visualization 4',
         subtitle = 'Data Visualization in Production with Shiny',
-        schedule = as.POSIXct('2023-04-24 11:30:00')
+        schedule = as.POSIXct('2023-04-24 13:30:00'),
+        timezone = Sys.timezone()
     )
 
     output$countdown <- renderUI({
         invalidateLater(250)
-        color <- ifelse(settings$schedule > Sys.time(), 'black', 'red')
+        schedule_tz <- force_tz(settings$schedule, settings$timezone)
+        color <- ifelse(schedule_tz > Sys.time(), 'black', 'red')
         remaining <- span(
-            round(as.period(abs(settings$schedule - Sys.time()))),
+            round(as.period(abs(schedule_tz - Sys.time()))),
             style = paste('color', color, sep = ':'))
         div(
             h1(settings$title),
             h2(settings$subtitle),
             h3('starts in'),
             h1(tags$b(remaining)),
-            h4(paste('at', settings$schedule, Sys.timezone())),
+            h4(paste('at', settings$schedule, settings$timezone)),
             class = 'center')
     })
 
@@ -31,6 +33,7 @@ server <- function(input, output, session) {
                 textInput("title", "Title", value = settings$title),
                 textInput("subtitle", "Subtitle", value = settings$subtitle),
                 airDatepickerInput("schedule", "Time", value = settings$schedule, timepicker = TRUE),
+                selectInput("timezone", "Timezone", choices = OlsonNames(), selected = settings$timezone),
                 footer = tagList(actionButton('settings_update', 'Update'))
             )
         )
@@ -38,20 +41,22 @@ server <- function(input, output, session) {
     observeEvent(input$settings_update, {
         settings$title <- input$title
         settings$subtitle <- input$subtitle
-        settings$schedule <- as.POSIXct(input$schedule)
+        settings$schedule <- as.POSIXct(input$schedule, tz = input$timezone)
+        settings$timezone <- input$timezone
         removeModal()
     })
 
     observe({
         query <- parseQueryString(session$clientData$url_search)
-        for (v in c('title', 'subtitle', 'schedule')) {
+        for (v in c('title', 'subtitle', 'schedule', 'timezone')) {
             if (!is.null(query[[v]])) {
                 if (v == 'schedule') {
-                    settings[[v]] <- as.POSIXct(query[[v]])
+                    settings[[v]] <- as.POSIXct(query[[v]], tz = settings$timezone)
                 } else {
                     settings[[v]] <- query[[v]]
                 }
             }
-        })
+        }
+    })
 
 }
