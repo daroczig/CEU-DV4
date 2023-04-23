563b0b84839caadf3f14defe869c729812c20ac0
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2023-04-23 12:34:39 +0200

    make current time reactive

--- apps/countdown/server.R
+++ apps/countdown/server.R
@@ -6,5 +6,11 @@ subtitle <- 'Data Visualization in Production with Shiny'
 server <- function(input, output) {
     output$title <- renderText(title)
     output$subtitle <- renderText(subtitle)
-    output$countdown <- renderText(Sys.time())
+    output$countdown <- renderText({
+        ## need to reset the cached/reactive value so that it gets updated from time to time,
+        ## as there's no external dependency triggering changes here (e.g. an input change)
+        ## that would automatically update this object to reactive
+        invalidateLater(250)
+        Sys.time()
+    })
 }
