487871771531ba696514c930eae4535ced642418
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2023-04-23 12:35:23 +0200

    show time in human-friendly format

--- apps/countdown/server.R
+++ apps/countdown/server.R
@@ -11,6 +11,6 @@ server <- function(input, output) {
         ## as there's no external dependency triggering changes here (e.g. an input change)
         ## that would automatically update this object to reactive
         invalidateLater(250)
-        Sys.time()
+        as.character(Sys.time())
     })
 }
