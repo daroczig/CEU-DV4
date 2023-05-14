efb38a4886e4a75641ab529ff3755c9e9117fce1
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2023-05-14 23:58:42 +0200

    add vertical whitespace

--- apps/ggplotgpt/ggplotgpt/ui.R
+++ apps/ggplotgpt/ggplotgpt/ui.R
@@ -8,6 +8,8 @@ fluidPage(
     textAreaInput('question', 'Hey, what do you want to know about the nycflights dataset?', height = '100px', width = '100%'),
     
     submitButton('Answer my question with a plot!', icon('image')),
+    
+    tags$br(),
     withSpinner(plotOutput('plot'), caption = 'Generating plot ...'),
     withSpinner(verbatimTextOutput('code'), caption = 'Hitting the OpenAI API for R code ...')
 
